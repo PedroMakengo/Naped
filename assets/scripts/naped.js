f2183bcf31b31c7388dfa4c929363608547e0f8b
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-09-21 12:31:11 -0300

    Trabalhando na página de error-found

--- assets/scripts/naped.js
+++ assets/scripts/naped.js
@@ -1,23 +1,3 @@
 $(function () {
   AOS.init();
-
-  $(".owl-carousel").owlCarousel({
-    rtl: true,
-    loop: true,
-    margin: 20,
-    nav: false,
-    autoplay: true,
-    smartSpeed: 2e3,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 3,
-      },
-    },
-  });
 });
